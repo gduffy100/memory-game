ba0b427f43d39c60ffe9f6a8dd28d888bc5809b5
Gerard Duffy <gduffy100@hotmail.com>
2020-04-10 16:39:28 +0100

    minor

--- app.js
+++ app.js
@@ -64,8 +64,14 @@ console.log("got Here")
 	}	
 	];
 	
+	cardArray.sort(() => 0.5 - math.Random());
+	
 	// create your game board
 	const grid = document.querySelector('.grid');
+	const resultDisplay = document.querySelector('#result');
+	var cardsChosen = [];
+	var cardsChosenId = [];
+	var cardsWon = [];
 	
 	function createBoard() {
 		for ( let i = 0; i < cardArray.length; i++) {
@@ -76,6 +82,42 @@ console.log("got Here")
 			grid.appendChild(card);
 		}		
 	}
+	
+	//check for matches
+	function checkForMatch() {
+		var cards =  document.querySelectorAll('img');
+		const optionOneId = cardsChosenId[0];
+		const optionTwoId = cardsChosenId[1];
+		if (cardsChosen[0] === cardsChosen[1]){
+			alert('you found a match');
+			cards[optionOneId].setAttribute('src','images/white.png');
+			cards[optionTwoId].setAttribute('src','images/white.png');
+			cardsWon.push(cardsChosen];
+		}else {
+			cards[optionOneId].setAttribute('src','images/blank.png');
+			cards[optionTwoId].setAttribute('src','images/blank.png');
+			alert('sorry, try again');
+		}
+		cardsChosen = [];
+		cardsChosenId = [];
+		resultDisplay.textContent = cardsWon.length;
+		if (cardsWon.length === cardArray.length/2){
+			resultDisplay.textContent = 'Congratulation! you found them all';
+		}
+	}
+	
+	//flip your card
+		function flipcard() {
+			var cardId = this.getAttribute('data-id');
+			cardsChosen.push(cardArray[cardId].name);
+			cardsChosenId.push(cardId);
+			this.setAttribute('src',cardArray[cardId].img);
+			if (cardsChosen.length === 2){
+				setTimeout(checkForMatch, 500);
+			}
+				
+		}
+	
 	
 	createBoard();
 
