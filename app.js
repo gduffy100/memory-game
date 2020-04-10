370e65d51fda4d4458cbc86140ff61a2f40718b5
Gerard Duffy <gduffy100@hotmail.com>
2020-04-10 15:53:02 +0100

    minor

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
-document.addEventListener('DOMContentLoaded', () = {
+if( document.readyState !== 'loading' ) {
+    console.log( 'document is already ready, just execute code here' );
+    myInitCode();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log( 'document was not ready, place code here' );
+        myInitCode();
+    });
+}
+
+function myInitCode() {
+
+console.log("got Here")
 	
 	//card options
 	const cardArray = [
 	{
-		name: "luigi-yoshi",
-		img: "images/luigi-yoshi.png"
+		name: 'luigi-yoshi',
+		img: 'images/luigi-yoshi.png'
 	},
 	{
-		name: "luigi-yoshi",
-		img: "images/luigi-yoshi.png"
+		name: 'luigi-yoshi',
+		img: 'images/luigi-yoshi.png'
 	},
 	{
-		name: "mario-fire-wizard",
-		img: "images/mario-fire-wizard.png"
+		name: 'mario-fire-wizard',
+		img: 'images/mario-fire-wizard.png'
 	},
 	{
-		name: "mario-fire-wizard",
-		img: "images/mario-fire-wizard.png"
+		name: 'mario-fire-wizard',
+		img: 'images/mario-fire-wizard.png'
 	},
 	{
-		name: "mario-kart",
-		img: "images/mario-kart.png"
+		name: 'mario-kart',
+		img: 'images/mario-kart.png'
 	},
 	{
-		name: "mario-kart",
-		img: "images/mario-kart.png"
+		name: 'mario-kart',
+		img: 'images/mario-kart.png'
 	},
 	{
-		name: "mario-magic-ball",
-		img: "images/mario-magic-ball.png"
+		name: 'mario-magic-ball',
+		img: 'images/mario-magic-ball.png'
 	},	
 		{
-		name: "mario-magic-ball",
-		img: "images/mario-magic-ball.png"
+		name: 'mario-magic-ball',
+		img: 'images/mario-magic-ball.png'
 	},	
 	{
-		name: "mushroom-head",
-		img: "images/mushroom-head.png"
+		name: 'mushroom-head',
+		img: 'images/mushroom-head.png'
 	},	
 	{
-		name: "mushroom-head",
-		img: "images/mushroom-head.png"
+		name: 'mushroom-head',
+		img: 'images/mushroom-head.png'
 	},	
 	{
-		name: "wii-tennis",
-		img: "images/wii-tennis.png"
+		name: 'wii-tennis',
+		img: 'images/wii-tennis.png'
 	},
 	{
-		name: "wii-tennis",
-		img: "images/wii-tennis.png"
+		name: 'wii-tennis',
+		img: 'images/wii-tennis.png'
 	}	
 	];
 	
@@ -67,4 +79,4 @@ document.addEventListener('DOMContentLoaded', () = {
 	
 	createBoard();
 
-})
+}
